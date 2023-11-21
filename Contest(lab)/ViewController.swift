77ae3ddc5b2d97fd8d9bbd51fa4d00ddd66e0282
spoons <ev4n.mus1c@gmail.com>
2023-11-21 09:31:39 -0700

    Initial Commit

---
+++ Contest(lab)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contest(lab)
+//
+//  Created by shark boy on 11/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
